7fc6aca2d73690e34c47eb450ae27a53b578f29a
mtuan <tuan.uet@gmail.com>
2016-10-09 20:49:55 +0700

    Fix all

--- app/src/main/java/vnu/uet/tuan/myuet/MainActivity.java
+++ app/src/main/java/vnu/uet/tuan/myuet/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
 
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.content_main2,new ListNotification())
+                    .replace(R.id.content_main,new ListNotification())
                     .commit();
         }
     }
@@ -37,14 +37,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
